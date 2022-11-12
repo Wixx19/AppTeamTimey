963571855bad045ce78b8b80e748682cf11d1787
William Garcia-Sanchez <89278772+Wixx19@users.noreply.github.com>
2022-11-12 14:26:25 -0500

    Add files via upload

---
+++ Timey/Timey/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  Timey
+//
+//  Created by William Garcia-Sanchez on 11/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject private var vm = ViewModel()
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    private let width: Double = 250
+    
+    var body: some View {
+        ZStack {
+            PlayerView()
+                .overlay(Color.blue.opacity(0.4))
+                .blur(radius: 1)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("\(vm.time)")
+                    .font(.system(size: 70, weight: .medium, design: .rounded))
+                    .alert("Timer done!", isPresented: $vm.showingAlert) {
+                        Button("Continue", role: .cancel) {
+                            // Code
+                        }
+                    }
+                    .padding()
+                    .frame(width: width)
+                    .background(.thinMaterial)
+                    .cornerRadius(20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.blue, lineWidth: 4)
+                    )
+                
+                Slider(value: $vm.minutes, in: 1...60, step: 1)
+                    .padding()
+                    .disabled(vm.isActive)
+                    .animation(.easeInOut, value: vm.minutes)
+                    .frame(width: width)
+                
+                HStack(spacing:50) {
+                    Button("Start") {
+                        vm.start(minutes: vm.minutes)
+                    }
+                    .disabled(vm.isActive)
+                    
+                    Button("Reset", action: vm.reset)
+                        .tint(.red)
+                }
+                .frame(width: width)
+            }
+            .onReceive(timer) { _ in
+                vm.updateCountdown()
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timey/Timey/TimeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeyApp.swift
+//  Timey
+//
+//  Created by William Garcia-Sanchez on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
